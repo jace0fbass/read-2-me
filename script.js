402b7a7a7c0ef1ce9d67ef24467c852d5b5c187f
Jason <jajones414@gmail.com>
2023-01-09 23:33:29 -0600

    Added tesseract.js.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const video = document.querySelector("video");
-const textElem = document.querySelector("[data-text")
+const textElem = document.querySelector("[data-text]")
 
 async function setup() {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   video.srcObject = stream;
 
-  video.addEventListener("playing", async() => {
+  video.addEventListener("playing", async () => {
     const worker = Tesseract.createWorker()
     await worker.load()
     await worker.loadLanguage("eng")
@@ -16,6 +16,7 @@ async function setup() {
     canvas.height = video.height
 
     document.addEventListener("keypress", async e => {
+        
         if(e.code !== "Space") return
         canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
         const {data: { text } } = await worker.recognize(canvas)
