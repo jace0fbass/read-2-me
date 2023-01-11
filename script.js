13c42372de3eeb67a2836b69c15e83dda318d5da
Jason <jajones414@gmail.com>
2023-01-10 22:06:01 -0600

    Updated kypress event to a click event, added css styling, and added a button to scan start the scan function.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const video = document.querySelector("video");
 const textElem = document.querySelector("[data-text]")
+const scan = document.getElementById("scan")
 
 async function setup() {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -15,9 +16,9 @@ async function setup() {
     canvas.width = video.width
     canvas.height = video.height
 
-    document.addEventListener("keypress", async e => {
+    document.addEventListener("click", async e => {
         
-        if(e.code !== "Space") return
+        if (e.type !== "click") return
         canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
         const { data: { text }, 
       } = await worker.recognize(canvas)
@@ -30,3 +31,7 @@ speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, " ")))
 }
 
 setup();
+
+// Choose back camera of phone
+// Change event listener to click & add a button
+// Style more
